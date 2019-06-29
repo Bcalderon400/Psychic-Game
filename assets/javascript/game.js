48e56b96e1380876a37a3171749adf19bf12be55
Brian Calderon <briancalderon200@gmail.com>
2019-06-28 21:48:11 -0700

    used different variables and added computer random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,14 @@
+var winCount = 0;
+var lossCount = 0;
+var remaining = 0;
+var guesses = 0;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var score = 0;
-var letterIndex = 0;
 
-for (i = 0; i < letters.length; i++) {
-    console.log("the letters:" + letters[i]);
-}
+var randomIndex = Math.floor(Math.random() * letters.length)
+var computerChoice = letters[randomIndex]
+
+console.log(computerChoice);
+
+//for (i = 0; i < letters.length; i++) {
+//  console.log("the letters:" + letters[i]);
+//}
