fc22d0e3cc64b19d44e8abc32fd5a5c7e23a7c94
Brian Calderon <briancalderon200@gmail.com>
2019-06-28 22:49:06 -0700

    fixed variable names, added userchoice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,24 @@
 var winCount = 0;
 var lossCount = 0;
-var remaining = 0;
-var guesses = 0;
+var guess = 0;
+var sofar = 0;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var randomIndex = Math.floor(Math.random() * letters.length)
 var computerChoice = letters[randomIndex]
 
 console.log(computerChoice);
+
+document.onkeyup = function(event) {
+    console.log(event);
+
+    var userChoice = event.key.toLowerCase();
+    if (letters.indexOf(userChoice) === -1) {
+        alert("Please enter a letter.")
+    } else {
+        console.log(userChoice)
+    }
+}
 
 //for (i = 0; i < letters.length; i++) {
 //  console.log("the letters:" + letters[i]);
